082db390ea67db1b53f1f03a4cce5e34b121b91e
Nithin-ns24 <99644046+Nithin-ns24@users.noreply.github.com>
2024-04-03 15:06:32 +0530

    Update BookServiceImpl.java

--- src/main/java/com/bittercode/service/impl/BookServiceImpl.java
+++ src/main/java/com/bittercode/service/impl/BookServiceImpl.java
@@ -16,7 +16,7 @@ import com.bittercode.util.DBUtil;
 
 public class BookServiceImpl implements BookService {
 
-    private static final String getAllBooksQuery = "SELECT * FROM " + BooksDBConstants.TABLE_BOOK;
+    private static final String getAllBooksQuery = "SELECT * FROM " + BooksDBConstants.TABLE_BOOK + " ORDER BY id DESC";;
     private static final String getBookByIdQuery = "SELECT * FROM " + BooksDBConstants.TABLE_BOOK
             + " WHERE " + BooksDBConstants.COLUMN_BARCODE + " = ?";
 
