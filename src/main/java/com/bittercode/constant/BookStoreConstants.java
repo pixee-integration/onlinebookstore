371607b982db61f2925a1655db27c54ec5785cca
Nithin-ns24 <99644046+Nithin-ns24@users.noreply.github.com>
2024-04-04 11:53:57 +0530

    Update BookStoreConstants.java

--- src/main/java/com/bittercode/constant/BookStoreConstants.java
+++ src/main/java/com/bittercode/constant/BookStoreConstants.java
@@ -1,7 +1,7 @@
 package com.bittercode.constant;
 
 public interface BookStoreConstants {
-	public static String CONTENT_TYPE_TEXT_HTML = "text/html";
+	#public static String CONTENT_TYPE_TEXT_HTML = "text/html";
 	
 	
 }
